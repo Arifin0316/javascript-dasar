8bee01a13ffa198a36eb277bc786485cdae836c3
Arifin0316 <masfin690@gmail.com>
2024-07-08 22:49:30 +0700

    Update game.js
    menambahkan game tebak angka

--- game.js
+++ game.js
@@ -34,6 +34,56 @@ tanya = confirm('lagi')
 
 alert('terima kasi suda bermain');
 
+
+// tebak angka
+
+
+alert('coba kalian tebak anka 1 - 10')
+var comp = Math.random()
+
+if(comp < 0.0){
+    comp = '1';
+}else if(comp >= 0.0 && comp <= 0.1){
+    comp = '2';
+}else if(comp >= 0.1 && comp <= 0.2){
+    comp = '3';
+}else if(comp >= 0.2 && comp <= 0.3){
+    comp = '4';
+}else if(comp >= 0.3 && comp <= 0.4){
+    comp = '5';
+}else if(comp >= 0.4 && comp <= 0.5){
+    comp = '6';
+}else if(comp >= 0.5 && comp <= 0.6){
+    comp = '7';
+}else if(comp >= 0.6 && comp <= 0.7){
+    comp = '8';
+}else if(comp >= 0.7 && comp <= 0.8){
+    comp = '8';
+}else if(comp >= 0.8 && comp <= 0.9){
+    comp = '9';
+}else{
+    comp = '10';
+}
+
+for(i = 1; i <= 3; i++){
+    var p = prompt('pili angka 1 sampai 10')
+    var hasil = '';
+    if( comp == p){
+        hasil = 'benar'
+    }else if(comp < p ){
+        hasil = 'terlalu besar'
+    }else if(comp > p ){
+        hasil = 'terlalu kecil'
+    }else{
+        alert('angka yang anda masukan salah')
+    }
+    alert(hasil)
+}
+alert('jawaban nya adalah ' + comp + ' terima kasi')
+
+
+
+
 // if(comp == 'orang'){
     //     hasil = 'menang';
     // }else{
